71e7b28b8b1992952443e90ec3b7f24303557b9b
flagersgit <flagersmail@gmail.com>
2020-11-12 05:29:27 -0500

    config: perry-lineage-171

--- config.sh
+++ config.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="flagersgit"
+export GITHUB_EMAIL="flagersmail@gmail.com"
 
-export device=""
+export device="perry"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="LineageOS"
+export ROM_DIR="/var/lib/jenkins/lineage171"
+export ROM_VERSION="17.1"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://github.com/flagersgit/manifests/raw/main/lineage/17.x/msm8937-common.xml"
+export manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage"
+export branch="lineage-17.1"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="clean"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="50"
 
-export jenkins="false"
+export jenkins="true"
 
 export release_repo=""
 
-export timezone="UTC"
+export timezone="America/New York"
+
